f8af4189fed1d0e5c5fbfb8bfbbb7d8e9d873d25
loveiska04 <nkate04@gmail.com>
2026-06-21 20:47:56 +0300

    my js-hw

--- js/task-3.js
+++ js/task-3.js
@@ -37,11 +37,3 @@ console.log(builder.getValue()); // "=^.^="
 
 
 
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
